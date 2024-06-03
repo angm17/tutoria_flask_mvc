ec5cf98d5afb5fa97f85dda4eb8a8c6702e6a389
Miguel Angarita <angarita.m17@gmail.com>
2024-06-02 21:41:35 -0500

    cambios hasta la creación y listado.

--- app/controllers/usuario_controller.py
+++ app/controllers/usuario_controller.py
@@ -1,9 +1,46 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
+
+from ..models import Usuario
+from .. import db
 
 
 usuario = Blueprint('usuario', __name__, static_folder='static')
 
 @usuario.route('/')
 def index():
-    # users = User.query.all()
-    return render_template('usuario.html', tituloPagina="Usuarios")
+    users = Usuario.query.all()
+    return render_template('usuario.html', tituloPagina="Usuarios", usuarios = users)
+
+@usuario.route('/guardar', methods=["POST"])
+def guardar():
+    nombres = request.form["nombres"]
+    estado = request.form["estado"]
+    
+    estado = bool(int(estado)) if estado is not None else False
+    
+
+    usuario = Usuario(
+        nombre = nombres,
+        estado = estado
+    )
+    
+    db.session.add(usuario)
+    
+    try:
+        db.session.commit()
+        return redirect("/usuarios")
+    except Exception as e:
+        db.session.rollback()
+    
+
+# def actualizar():
+#     pass
+
+# def eliminar():
+#     pass
+
+# def listar():
+#     pass
+
+# def listarTodos():
+#     pass
